85f3b3633581ba29a276c48538682a61025c2ef7
Toubeyas <49030158+Toubeyas@users.noreply.github.com>
2019-04-08 21:27:27 +0200

    Added all menus

--- app.js
+++ app.js
@@ -1,11 +1,1145 @@
 function testing(){
   console.log("Hello world timing test");
 }
-var Zapfhahn=[
-  {
-    "name":"Hoepfner",
-    "price":3.2,
-    "type":0
-  }
-]
-;
+var Enchilada = [
+				{
+					"name":"Baileys Colada",
+					"price":6.9,
+					"type":4,
+					"happyHour":4.2,
+					"enchiHour":0,
+					"cocktailWürfeln":1
+				},
+				{
+					"name":"Pina Colada",
+					"price":6.9,
+					"type":4,
+					"happyHour":4.2,
+					"enchiHour":0,
+					"cocktailWürfeln":1
+				},
+				{
+					"name":"Caipirinha",
+					"price":6.6,
+					"type":4,
+					"happyHour":4.2,
+					"enchiHour":0,
+					"cocktailWürfeln":1
+				},
+				{
+					"name":"Caipi Melon",
+					"price":6.9,
+					"type":4,
+					"happyHour":4.2,
+					"enchiHour":0,
+					"cocktailWürfeln":1
+				},
+				{
+					"name":"Caribbean Caipi",
+					"price":6.9,
+					"type":4,
+					"happyHour":4.2,
+					"enchiHour":0,
+					"cocktailWürfeln":1
+				},
+				{
+					"name":"Mojito",
+					"price":6.9,
+					"type":4,
+					"happyHour":4.2,
+					"enchiHour":0,
+					"cocktailWürfeln":1
+				},
+				{
+					"name":"Strawberry Mojito",
+					"price":7.4,
+					"type":4,
+					"happyHour":4.2,
+					"enchiHour":0,
+					"cocktailWürfeln":1
+				},
+				{
+					"name":"Gin Fizz",
+					"price":6.4,
+					"type":4,
+					"happyHour":4.2,
+					"enchiHour":0,
+					"cocktailWürfeln":1
+				},
+				{
+					"name":"Singapore Sling",
+					"price":6.8,
+					"type":4,
+					"happyHour":4.2,
+					"enchiHour":0,
+					"cocktailWürfeln":1
+				},
+				{
+					"name":"Sex on the Beach",
+					"price":6.8,
+					"type":4,
+					"happyHour":4.2,
+					"enchiHour":0,
+					"cocktailWürfeln":1
+				},
+				{
+					"name":"Touch Down",
+					"price":6.8,
+					"type":4,
+					"happyHour":4.2,
+					"enchiHour":0,
+					"cocktailWürfeln":1
+				},
+				{
+					"name":"Pisco Sour",
+					"price":6.9,
+					"type":4,
+					"happyHour":4.2,
+					"enchiHour":0,
+					"cocktailWürfeln":1
+				},
+				{
+					"name":"Whiskey Sour",
+					"price":6.9,
+					"type":4,
+					"happyHour":4.2,
+					"enchiHour":0,
+					"cocktailWürfeln":1
+				},
+				{
+					"name":"Espresso Martini",
+					"price":6.4,
+					"type":4,
+					"happyHour":4.2,
+					"enchiHour":0,
+					"cocktailWürfeln":1
+				},
+				{
+					"name":"Mexican Lipstick",
+					"price":6.9,
+					"type":4,
+					"happyHour":4.2,
+					"enchiHour":0,
+					"cocktailWürfeln":1
+				},
+				{
+					"name":"Big Ben",
+					"price":7.8,
+					"type":4,
+					"happyHour":6.0,
+					"enchiHour":6.0,
+					"cocktailWürfeln":1
+				},
+				{
+					"name":"Captain's Hurricane",
+					"price":7.9,
+					"type":4,
+					"happyHour":6.0,
+					"enchiHour":6.0,
+					"cocktailWürfeln":1
+				},
+				{
+					"name":"Cloud 9",
+					"price":7.8,
+					"type":4,
+					"happyHour":6.0,
+					"enchiHour":6.0,
+					"cocktailWürfeln":1
+				},
+				{
+					"name":"Enchilada Cooler",
+					"price":7.8,
+					"type":4,
+					"happyHour":6.0,
+					"enchiHour":6.0,
+					"cocktailWürfeln":1
+				},
+				{
+					"name":"Long Island Ice Tea",
+					"price":7.8,
+					"type":4,
+					"happyHour":6.0,
+					"enchiHour":6.0,
+					"cocktailWürfeln":1
+				},
+				{
+					"name":"Long Island Mint",
+					"price":8.5,
+					"type":4,
+					"happyHour":6.0,
+					"enchiHour":6.0,
+					"cocktailWürfeln":1
+				},
+				{
+					"name":"Long Island Bull",
+					"price":8.5,
+					"type":4,
+					"happyHour":6.0,
+					"enchiHour":6.0,
+					"cocktailWürfeln":1
+				},
+				{
+					"name":"Long Paloma",
+					"price":8.5,
+					"type":4,
+					"happyHour":6.5,
+					"enchiHour":6.5,
+					"cocktailWürfeln":1
+				}
+			];
+
+			
+
+			var Altbaden = [
+				{
+					"name":"Shot",
+					"price":1.0,
+					"type":3
+				},
+				{
+					"name":"Terrorschorle",
+					"price":3.7,
+					"type":2
+				}
+			];
+
+
+			var Zapfhahn = [
+				/*{
+					"food":[
+						{
+							"name":"Pizza Margherita",
+							"price":4.5,
+							"type":1
+						},
+						{
+							"name":"Pizza 4 Jahreszeiten",
+							"price":6.9,
+							"type":1
+						},
+						{
+							"name":"Pizza alla Chef",
+							"price":7.2,
+							"type":1
+						},
+						{
+							"name":"Pizza Thunfisch",
+							"price":6.9,
+							"type":1
+						},
+						{
+							"name":"Flammkuchen alla Chef",
+							"price":7.9,
+							"type":1
+						},
+						{
+							"name":"Italienischer Salat",
+							"price":6.9,
+							"type":1
+						}
+					]
+				},
+				{
+					"beer":[
+						{
+							"name":"Pilsner 0,3l",
+							"price":2.4,
+							"type":0
+						},
+						{
+							"name":"Pilsner 0,4l",
+							"price":2.9,
+							"type":0
+						},
+						{
+							"name":"Export 0,4l",
+							"price":2.9,
+							"type":0
+						},
+						{
+							"name":"Kräusen 0,3l",
+							"price":2.4,
+							"type":0
+						},
+						{
+							"name":"Kräusen 0,4l",
+							"price":2.9,
+							"type":0
+						},
+						{
+							"name":"Radler 0,4l",
+							"price":2.4,
+							"type":0
+						},
+						{
+							"name":"Maß",
+							"price":7.0,
+							"type":0
+						},
+						{
+							"name":"Bananenweizen 0,5l",
+							"price":3.7,
+							"type":0
+						},
+						{
+							"name":"Kristall Weizen 0,5l",
+							"price":2.9,
+							"type":0
+						},
+						{
+							"name":"Helles Weizen 0,5l",
+							"price":2.9,
+							"type":0
+						},
+						{
+							"name":"Dunkles Weizen 0,5l",
+							"price":2.9,
+							"type":0
+						},
+						{
+							"name":"Grape 0,33l",
+							"price":2.4,
+							"type":0
+						}
+					]
+				},
+				{
+					"whine":
+				},*/
+				
+				{
+					"name":"Pizza Margherita",
+					"price":4.5,
+					"type":1
+				},
+				{
+					"name":"Pizza 4 Jahreszeiten",
+					"price":6.9,
+					"type":1
+				},
+				{
+					"name":"Pizza alla Chef",
+					"price":7.2,
+					"type":1
+				},
+				{
+					"name":"Pizza Thunfisch",
+					"price":6.9,
+					"type":1
+				},
+				{
+					"name":"Flammkuchen alla Chef",
+					"price":7.9,
+					"type":1
+				},
+				{
+					"name":"Italienischer Salat",
+					"price":6.9,
+					"type":1
+				},
+				{
+					"name":"Pilsner 0,3l",
+					"price":2.4,
+					"type":0
+				},
+				{
+					"name":"Pilsner 0,4l",
+					"price":2.9,
+					"type":0
+				},
+				{
+					"name":"Export 0,4l",
+					"price":2.9,
+					"type":0
+				},
+				{
+					"name":"Kräusen 0,3l",
+					"price":2.4,
+					"type":0
+				},
+				{
+					"name":"Kräusen 0,4l",
+					"price":2.9,
+					"type":0
+				},
+				{
+					"name":"Radler 0,4l",
+					"price":2.4,
+					"type":0
+				},
+				{
+					"name":"Maß",
+					"price":7.0,
+					"type":0
+				},
+				{
+					"name":"Bananenweizen 0,5l",
+					"price":3.7,
+					"type":0
+				},
+				{
+					"name":"Kristall Weizen 0,5l",
+					"price":2.9,
+					"type":0
+				},
+				{
+					"name":"Helles Weizen 0,5l",
+					"price":2.9,
+					"type":0
+				},
+				{
+					"name":"Dunkles Weizen 0,5l",
+					"price":2.9,
+					"type":0
+				},
+				{
+					"name":"Grape 0,33l",
+					"price":2.4,
+					"type":0
+				},
+				{
+					"name":"Ita. Landwein 0,2l",
+					"price":2.8,
+					"type":5
+				},
+				{
+					"name":"Lambrusco 0,25l",
+					"price":2.8,
+					"type":5
+				},
+				{
+					"name":"Havana Club",
+					"price":4.5,
+					"type":2
+				},
+				{
+					"name":"Asbach-Cola",
+					"price":3.5,
+					"type":2
+				},
+				{
+					"name":"Bacardi-Cola",
+					"price":3.5,
+					"type":2
+				},
+				{
+					"name":"Vodka-Lemon",
+					"price":3.5,
+					"type":2
+				},
+				{
+					"name":"Vodka-Energy",
+					"price":3.5,
+					"type":2
+				},
+				{
+					"name":"Captain-Cola",
+					"price":4.5,
+					"type":2
+				},
+				{
+					"name":"Gin Tonic",
+					"price":3.5,
+					"type":2
+				},
+				{
+					"name":"Tequila",
+					"price":2.3,
+					"type":3
+				},
+				{
+					"name":"Tobinambur",
+					"price":2.3,
+					"type":3
+				},
+				{
+					"name":"Vodka",
+					"price":2.3,
+					"type":3
+				},
+				{
+					"name":"Ouzo",
+					"price":2.3,
+					"type":3
+				},
+				{
+					"name":"Jägermeister",
+					"price":2.3,
+					"type":3
+				},
+				{
+					"name":"Saurer Apfel",
+					"price":2.0,
+					"type":3
+				},
+				{
+					"name":"Apfelkorn",
+					"price":2.0,
+					"type":3
+				},
+				{
+					"name":"Obstler",
+					"price":1.9,
+					"type":3
+				},
+				{
+					"name":"1-Meter Flammkuchen",
+					"price":20.5,
+					"type":1
+				},
+				{
+					"name":"1-Meter Pizza ",
+					"price":19.8,
+					"type":1
+				},
+				{
+					"name":"1-Meter Bier",
+					"price":16.4,
+					"type":0
+				},
+				{
+					"name":"3l Bier Tower",
+					"price":19,
+					"type":0
+				},
+				{
+					"name":"5l Bier Tower",
+					"price":30,
+					"type":0
+				}
+			];
+
+			var Bierbrezel = [
+			
+				{
+					"name":"Dinkelacker Pils 0,2l",
+					"price":1.7,
+					"type":0
+				},
+				{
+					"name":"Dinkelacker Pils 0,3l",
+					"price":2.4,
+					"type":0
+				},
+				{
+					"name":"Dinkelacker Pils 0,4l",
+					"price":2.6,
+					"type":0
+				},
+				{
+					"name":"Hefeweizen 0,3l",
+					"price":2.4,
+					"type":0
+				},
+				{
+					"name":"Hefeweizen 0,5l",
+					"price":2.9,
+					"type":0
+				},
+				{
+					"name":"Naturtrüb 0,3l",
+					"price":2.5,
+					"type":0
+				},
+				{
+					"name":"Naturtrüb 0,5l",
+					"price":3.5,
+					"type":0
+				},
+				{
+					"name":"Maß",
+					"price":6.5,
+					"type":0
+				},
+				{
+					"name":"Guinness 0,3l",
+					"price":2.8,
+					"type":0
+				},
+				{
+					"name":"Guinness 0,4l",
+					"price":3.4,
+					"type":0
+				},
+				{
+					"name":"Guinness 0,5l",
+					"price":4.2,
+					"type":0
+				},
+				{
+					"name":"Radler 0,2l",
+					"price":1.7,
+					"type":0
+				},
+				{
+					"name":"Radler 0,3l",
+					"price":2.4,
+					"type":0
+				},
+				{
+					"name":"Radler 0,4l",
+					"price":2.6,
+					"type":0
+				},
+				{
+					"name":"Radler 0,5l",
+					"price":2.9,
+					"type":0
+				},
+				{
+					"name":"Andres Radler 0,3l",
+					"price":2.3,
+					"type":0
+				},
+				{
+					"name":"Andres Radler 0,5l",
+					"price":3.3,
+					"type":0
+				},
+				{
+					"name":"Weizenradler 0,5l",
+					"price":2.9,
+					"type":0
+				},
+				{
+					"name":"Cola Weizen 0,5l",
+					"price":2.9,
+					"type":0
+				},
+				{
+					"name":"Bananen Weizen 0,5l",
+					"price":2.9,
+					"type":0
+				},
+				{
+					"name":"Export 0,5l",
+					"price":2.6,
+					"type":0
+				},
+				{
+					"name":"Rothaus 0,33l",
+					"price":2.6,
+					"type":0
+				},
+				{
+					"name":"Kristallweizen 0,5l",
+					"price":2.9,
+					"type":0
+				},
+				{
+					"name":"Jim Beam",
+					"price":3.4,
+					"type":2
+				},
+				{
+					"name":"Jack Daniel's",
+					"price":3.4,
+					"type":2
+				},
+				{
+					"name":"Johnny Walker",
+					"price":3.4,
+					"type":2
+				},
+				{
+					"name":"Jim Beam + Cola",
+					"price":4.0,
+					"type":2
+				},
+				{
+					"name":"Jack Daniel's + Cola",
+					"price":4.0,
+					"type":2
+				},
+				{
+					"name":"Johnny Walker + Cola",
+					"price":4.0,
+					"type":2
+				},
+				{
+					"name":"Shot (außer Williams Birne)",
+					"price":2.3,
+					"type":3
+				},
+				{
+					"name":"Williams Birne Pircher",
+					"price":2.6,
+					"type":3
+				}
+			];
+
+			var Cash = [
+				/*{
+					"name":"Kaffee",
+					"price":2.0,
+					"type":0
+				},
+				{
+					"name":"Espresso",
+					"price":2.0,
+					"type":0
+				},
+				{
+					"name":"Tee",
+					"price":1.8,
+					"type":0
+				},*/
+				{
+					"name":"Becks's 0,33l",
+					"price":2.6,
+					"type":0
+				},
+				{
+					"name":"Beck's Gold 0,33l",
+					"price":2.6,
+					"type":0
+				},
+				{
+					"name":"Heineken 0,2l",
+					"price":2.6,
+					"type":0
+				},
+				{
+					"name":"Diebels 0,33l",
+					"price":2.6,
+					"type":0
+				},
+				{
+					"name":"Grape 0,33l",
+					"price":2.6,
+					"type":0
+				},
+				{
+					"name":"Tannenzäpfle",
+					"price":2.6,
+					"type":0
+				},
+				{
+					"name":"Desperados",
+					"price":3.6,
+					"type":0
+				},
+				{
+					"name":"Radler 0,33l",
+					"price":2.6,
+					"type":0
+				},
+				{
+					"name":"Cola Bier",
+					"price":2.6,
+					"type":0
+				},
+				{
+					"name":"Kristallweizen 0,5l",
+					"price":3.0,
+					"type":0
+				},
+				{
+					"name":"Jack Daniel's Long Drink",
+					"price":3.3,
+					"type":2
+				},
+				{
+					"name":"Johnny Walker Long Drink",
+					"price":3.3,
+					"type":2
+				},
+				{
+					"name":"Jim Beam Long Drink",
+					"price":3.3,
+					"type":2
+				},
+				{
+					"name":"Chivas Regal Long Drink",
+					"price":3.5,
+					"type":2
+				},
+				{
+					"name":"Asbach Long Drink",
+					"price":3.0,
+					"type":2
+				},
+				{
+					"name":"Bacardi Long Drink",
+					"price":3.0,
+					"type":2
+				},
+				{
+					"name":"Wodka Long Drink",
+					"price":2.6,
+					"type":2
+				},
+				{
+					"name":"Wodka Red Bull",
+					"price":4.0,
+					"type":2
+				},
+				{
+					"name":"Havanna Cola",
+					"price":4.0,
+					"type":2
+				},
+				{
+					"name":"Southern Comfort Long Drink",
+					"price":3.3,
+					"type":2
+				},
+				{
+					"name":"Metaxa Long Drink",
+					"price":3.2,
+					"type":2
+				},
+				{
+					"name":"Glenfiddich Long Drink",
+					"price":3.5,
+					"type":2
+				},
+				{
+					"name":"Malibu Long Drink",
+					"price":3.0,
+					"type":2
+				},
+				{
+					"name":"Tequila",
+					"price":2.2,
+					"type":3
+				},
+				{
+					"name":"Ouzo",
+					"price":2.2,
+					"type":3
+				},
+				{
+					"name":"Jägermeister",
+					"price":2.2,
+					"type":3
+				},
+				{
+					"name":"Ficken",
+					"price":2.2,
+					"type":3
+				},
+				{
+					"name":"Sambuca",
+					"price":2.2,
+					"type":3
+				},
+				{
+					"name":"Ramazotti",
+					"price":3.2,
+					"type":3
+				},
+				{
+					"name":"Baileys",
+					"price":3.5,
+					"type":3
+				},
+				{
+					"name":"Williams",
+					"price":2.2,
+					"type":3
+				},
+				{
+					"name":"Wodka Waldmeister",
+					"price":2.2,
+					"type":3
+				},
+				{
+					"name":"Wein 0,2l",
+					"price":2.8,
+					"type":5
+				},
+				{
+					"name":"Weinschorle 0,2l",
+					"price":2.5,
+					"type":5
+				},
+				{
+					"name":"Sekt Hausmarke 0,1l",
+					"price":2.5,
+					"type":5
+				},
+				{
+					"name":"Sekt Hausmarke (Flasche) 0,7l",
+					"price":18.0,
+					"type":5
+				},
+				{
+					"name":"Prosecco / Hausmarke 0,1l",
+					"price":2.8,
+					"type":5
+				},
+				{
+					"name":"Sekt / Prosecco auf Eis 0,2l",
+					"price":3.5,
+					"type":5
+				},
+				{
+					"name":"Sekt Aperol / Crodino 0,2l",
+					"price":4.5,
+					"type":5
+				},
+				{
+					"name":"Piccolo Geldermann 0,2l",
+					"price":5.9,
+					"type":5
+				},
+				{
+					"name":"Piccolo Hausmarke 0,2l",
+					"price":4.5,
+					"type":5
+				},
+				{
+					"name":"Cash-Baguette",
+					"price":4.5,
+					"type":1
+				}
+			];
+
+			var Freiraum = [
+				{
+					"name":"Freiraum-Baguette",
+					"price":4.5,
+					"type":1
+				},
+				{
+					"name":"Pils 0,3l",
+					"price":2.3,
+					"type":0
+				},
+				{
+					"name":"Pils 0,4l",
+					"price":2.7,
+					"type":0
+				},
+				{
+					"name":"Pils 0,5l",
+					"price":3.1,
+					"type":0
+				},
+				{
+					"name":"Hefeweizen 0,3l",
+					"price":2.3,
+					"type":0
+				},
+				{
+					"name":"Hefeweizen 0,5l",
+					"price":3.1,
+					"type":0
+				},
+				{
+					"name":"Kölsch 0,2l",
+					"price":1.8,
+					"type":0
+				},
+				{
+					"name":"Kölsch 11er Kranz",
+					"price":17.0,
+					"type":0
+				},
+				{
+					"name":"Radler 0,4l",
+					"price":2.7,
+					"type":0
+				},
+				{
+					"name":"Radler 0,5l",
+					"price":3.1,
+					"type":0
+				},
+				{
+					"name":"Colabier 0,4l",
+					"price":2.7,
+					"type":0
+				},
+				{
+					"name":"Colabier 0,5l",
+					"price":3.1,
+					"type":0
+				},
+				{
+					"name":"Colaweizen 0,3l",
+					"price":2.3,
+					"type":0
+				},
+				{
+					"name":"Colaweizen 0,5l",
+					"price":3.1,
+					"type":0
+				},
+				{
+					"name":"Weizenradler 0,3l",
+					"price":2.3,
+					"type":0
+				},
+				{
+					"name":"Weizenradler 0,5l",
+					"price":3.1,
+					"type":0
+				},
+				{
+					"name":"Kristallweizen 0,5l",
+					"price":3.1,
+					"type":0
+				},
+				{
+					"name":"Hefeweizen Dunkel 0,5l",
+					"price":3.1,
+					"type":0
+				},
+				{
+					"name":"Tannenzäpfle 0,33l",
+					"price":2.6,
+					"type":0
+				},
+				{
+					"name":"Mixery 0,33l",
+					"price":2.6,
+					"type":0
+				},
+				{
+					"name":"Grape 0,33l",
+					"price":2.6,
+					"type":0
+				},
+				{
+					"name":"Desperados 0,33l",
+					"price":3.0,
+					"type":0
+				},
+				{
+					"name":"Corona 0,33l",
+					"price":3.0,
+					"type":0
+				},
+				{
+					"name":"Riesling 0,2l",
+					"price":3.8,
+					"type":5
+				},
+				{
+					"name":"Rose 0,2l",
+					"price":3.8,
+					"type":5
+				},
+				{
+					"name":"Dornfelder 0,2l",
+					"price":3.8,
+					"type":5
+				},
+				{
+					"name":"Weinschorle 0,2l",
+					"price":2.5,
+					"type":5
+				},
+				{
+					"name":"Weinschorle 0,4l",
+					"price":4.5,
+					"type":5
+				},
+				{
+					"name":"Jacky Cola",
+					"price":4.2,
+					"type":2
+				},
+				{
+					"name":"Havana Cola",
+					"price":4.2,
+					"type":2
+				},
+				{
+					"name":"Johnnie Cola",
+					"price":4.2,
+					"type":2
+				},
+				{
+					"name":"Gin Tonic",
+					"price":4.2,
+					"type":2
+				},
+				{
+					"name":"Vodka Lemon",
+					"price":4.2,
+					"type":2
+				},
+				{
+					"name":"Asbach Cola",
+					"price":4.2,
+					"type":2
+				},
+				{
+					"name":"Captain Cola",
+					"price":4.2,
+					"type":2
+				},
+				{
+					"name":"Jim Beam Cola",
+					"price":4.2,
+					"type":2
+				},
+				{
+					"name":"Tequila",
+					"price":2.2,
+					"type":3
+				},
+				{
+					"name":"Jägermeister",
+					"price":2.2,
+					"type":3
+				},
+				{
+					"name":"Ouzo",
+					"price":2.2,
+					"type":3
+				},
+				{
+					"name":"Sambuca",
+					"price":2.2,
+					"type":3
+				},
+				{
+					"name":"Ramazotti",
+					"price":2.2,
+					"type":3
+				},
+				{
+					"name":"Grappa",
+					"price":2.2,
+					"type":3
+				},
+				{
+					"name":"Obstler",
+					"price":2.2,
+					"type":3
+				},
+				{
+					"name":"Saurer Joster",
+					"price":1.5,
+					"type":3
+				},
+				{
+					"name":"Caipirinha",
+					"price":5.5,
+					"type":4
+				},
+				{
+					"name":"Sex on the Beach",
+					"price":6.5,
+					"type":4
+				},
+				{
+					"name":"Long Island IceTea",
+					"price":6.5,
+					"type":4
+				},
+				{
+					"name":"Pina Colada",
+					"price":6.5,
+					"type":4
+				},
+				{
+					"name":"Mai Tai",
+					"price":6.5,
+					"type":4
+				}
+			];
